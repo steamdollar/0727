25ce7624b021ed3137fbeab97326f1b58dbd68b1
이승준 <chronica@iseungjun-ui-MacBookAir.local>
2022-07-27 14:03:57 +0900

    1

--- truffle/contracts/abcToken.sol
+++ truffle/contracts/abcToken.sol
@@ -3,19 +3,64 @@ pragma solidity ^0.8.15;
 
 import "./node_modules/openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "./node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
+import "./node_modules/openzeppelin-solidity/contracts/utils/Strings.sol";
 
 contract abcToken is ERC721Enumerable, Ownable {
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
 
+    uint constant public MAX_TOKEN_COUNT = 1000;
+    // 최대 발행 갯수
+
+    uint public mint_price = 1 ether;
+    // 돈의 단위를 적을 때 10**18을 직접 쓰면 연산때문에 돈이 나간다
+    // 그래서 솔리디티에서 단위를 만들었음.. 알아서 1 ether를 10^18로 바꿔준다
+
+    string public metadataURI; 
+
+    constructor(string memory _name, string memory _symbol, string memory _metadataURI) ERC721(_name, _symbol) {
+        metadataURI = _metadataURI;
     }
 
-    function mintToken() public {
+    struct TokenData {
+        uint Rank;
+        uint Type;
+    }
+
+    mapping(uint => TokenData) public TokenDatas;
+
+    function mintToken() public payable{
+        require(msg.value >= mint_price);
+        require(MAX_TOKEN_COUNT > totalSupply());
         uint tokenId = totalSupply() + 1;
         // totalSupply함수는 erc721Enu에 있다.
+        // TokenData memory random = getRandomNum(msg.sender, tokenId);
+        TokenDatas[tokenId] = getRandomNum(msg.sender, tokenId);
+
+        payable(Ownable.owner()).transfer(msg.value); // owner는 컨트랙트 배포자 Ownable 컨트랙트에 있음
         _mint(msg.sender, tokenId);
     }
 
-    function tokenURI(uint tokenId) public override view returns (string memory) {
+    function tokenURI(uint _tokenId) public override view returns (string memory) {
+        // tokenId > rank, type  
+        // e.g. 1 > (1,4)
+        // return baseurl/metadata/1/4.json
+        // ipfs > 파일을 블록체인에 저장 : 파일 조각을 동시에 여러 노드에서 가져옴 faster than http
+        // http://www.pinata.cloud/
+        string memory Rank = Strings.toString(TokenDatas[_tokenId].Rank);
+        string memory Type = Strings.toString(TokenDatas[_tokenId].Type);
+        return string(abi.encodePacked(metadataURI,"/", Rank, "/", Type, ".json"));
+    }
 
+    function getRandomNum (address _owner, uint _tokenId) private pure returns(TokenData memory) {
+        // Random
+        // encodePacked 타입과 상관없이 변수들을 합쳐줌
+        uint randomNum = uint(keccak256(abi.encodePacked(_owner, _tokenId))) % 100;
+        TokenData memory data;
+
+        data.Type = 1;
+        data.Rank = 1;
+        // 메모리 상으로 data 변수를 만들었고, 이건 랭크랑 타입을 만드는데 사용된 후,
+        // 함수가 값을 리턴하면 사라짐
+        // 대충 data를 조건문에 따라 경우를 나눠 만들었다치자.
+        return data;
     }
 }
--- zmemo.md
+++ zmemo.md
@@ -28,3 +28,56 @@ mintTOken, tokenURI 함수를 쓰는데
 tokenURi는 상속받아 오되, 내가 만들고 싶은대로 고쳐서 쓸거기 때문에 override 넣어줘야 함
 
 mintToken은 그냥 내가 새로 선언한 함수
+
+//
+
+mintToken 먼저 만들고
+
+tokenURI 만들어보자
+
+이걸 넣으면 배포한 느프트가 보일거고 등등..
+
+json meatdata..
+
+딱히 표준은 없고 그냥 큰 시장의 권장 사항을 따라감
+
+아무튼 각각의 tokenURI를 발행 느프트의 수가 많을때는 어떻게 편하게 만드냐
+
+tokenId를 기준으로 중간 매개체 역할을 해줄 구조체 struct를 하나 만들거다.
+
+//
+
+우리가 nft를 살 때 레어도에 따라 나올 확률이 다르다.
+
+같은 레어도에도 여러가지 느프트가 있을거고..
+
+https://github.com/ingoo-blockchain/NFT_Mint/tree/master/public
+
+//
+
+아무튼 TokenData 구조체를 만들고 이걸 token과 연결한 TokenDatas 매핑을 만든다. 
+
+이 구조체 각각의 값을 정하기 위해 getRandomNum 함수를 작성한다.
+
+했으면 mintToken 함수에서 호출해주면 됨
+
+tokenURI gkatndptj 특정 토큰의 정보를 가져와 uri를 만들어주는데
+
+데이터 타입이 달라서 변환을 하기가 귀찮다.
+
+그래서 라이브러리가 있다.
+
+오픈 제펠린 > utils/String.sol에 있다.
+
+//
+
+생성자 함수에 배포를 진행할때의 베이스 url을 넣는다.
+
+얘는 ERC721에 있는건 아니므로 상속값은 아님
+
+https://gateway.pinata.cloud/ipfs/QmUsEKtVS5Gn4rZWbYfD7D4qLLKPf1YbsBWYaSqtmCPBzf
+
+일단 이걸로 쓴다..
+
+remixd -s . --remix-ide https://remix.ethereum.org
+
